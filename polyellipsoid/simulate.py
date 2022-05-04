307aca06c64fd7c02244f68dddcfaf2487eab3e7
chrisjonesBSU <chrisjones4@boisestate.edu>
2022-05-03 23:26:21 -0600

    fix doc strings

--- polyellipsoid/simulate.py
+++ polyellipsoid/simulate.py
@@ -100,20 +100,7 @@ class Simulation:
         Parameters
         ----------
         kT : float, required
-            T        sim = Simulation(
-                system=packed_system,
-                lperp=1.0,
-                lpar=1.0,
-                epsilon=1.0,
-                tau=0.1,
-                dt=0.001,
-                r_cut=2.0,
-                bond_k=1000,
-                bond_r0=0.25,
-                seed=42,
-                gsd_write=1000,
-                log_write=100
-        )emperature during shrink steps
+            Temperature during shrink steps
         n_steps : int, required
             Number of simulations steps during shrinking
         shrink_period : int, optional, default=10
