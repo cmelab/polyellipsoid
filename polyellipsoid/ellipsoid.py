7a5472f620add05b9fc4e3c48c08cbb5a7767e79
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2022-10-27 15:16:01 -0600

    remove middle C particles

--- polyellipsoid/ellipsoid.py
+++ polyellipsoid/ellipsoid.py
@@ -2,32 +2,19 @@ from mbuild import Compound
 
 
 class Ellipsoid(Compound):
-    def __init__(
-            self, mass, length, name="dimer"
-    ):
+    def __init__(self, mass, length, name="dimer"):
         super(Ellipsoid, self).__init__(name=name)
         self.length = float(length)
         # Create the constituent particles
         self.head = Compound(
                 pos=[0, 0, self.length/2],
                 name="CH",
-                mass=mass/2
+                mass=mass/4
         )
         self.tail = Compound(
                 pos=[0, 0, -self.length/2],
                 name="CT",
-                mass=mass/2
-        )
-        self.mid_head = Compound(
-                pos=[0,0,self.length/4]
-                name="CHM"
-                mass=0
-        )
-        self.mid_tail = Compound(
-                pos=[0,0,-self.length/4]
-                name="CTM"
-                mass=0
+                mass=mass/4
         )
         self.add([self.head, self.tail])
-        self.add_bond([self.head, self.mid_head])
-        self.add_bond([self.tail, self.mid_tail])
+        self.add_bond([self.head, self.tail])
